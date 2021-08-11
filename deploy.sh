25157acb42ab5c9b0e1f35a2fe7a5d33ce30b5fe
Shu-Wei <shuweidesigns@gmail.com>
2021-08-11 20:18:09 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:shudesigns/chts-test.git main:gh-pages
+git push -f https://github.com/shudesigns/chts-test.git master
 
 cd -
